b3a4ebc3f2c2e66249a51de8d2c5a5e5229e761a
j129008 <j129008@gmail.com>
2015-06-17 20:04:44 +0800

    first ci

---
+++ hello.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+import seed
+#import nltk
+#f = open("tpp.np.txt", "r", encoding="UTF-8")
+#drcstr = f.read()
+seed.printme7("tpp.np.txt","smile")
+#drctext = nltk.Text(drcstr.split())
+#drctext.concordance("寶玉", width=20)
---
+++ seed.py
@@ -0,0 +1,28 @@
+# -*- coding: UTF-8 -*-
+import re
+import sys
+def printme(filename, endline):
+    inFile = open(filename,"r",encoding="UTF-8")
+    i = 0
+    for line in inFile:
+        if len(line) >= 0 and (re.match("^(\s)+$", line) == None):
+            sys.stdout.write(i)
+            print(line)
+            i= i+1
+            if i == endline:
+                break
+    inFile.close()
+
+def printme7(filename, keyword):
+    inFile = open(filename,"r",encoding="UTF-8")
+    for line in inFile:
+        if (keyword in line ):
+            print(line)
+    inFile.close()
+
+def countJJ(filename, keyword):
+    inFile = open(filename,"r",encoding="UTF-8")
+    for line in inFile:
+        if (keyword in line ):
+            print(line)
+    inFile.close()
